40878d50a574761ab283bb25f81ab38f25274a65
daniel 'kirplink' reynolds <=>
2018-11-01 16:02:54 -0500

    uploading basic files

---
+++ assets/js/game.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyBdsHwUZA0x41BRkzae9HGmlp195SyVvuw",
+    authDomain: "rps-firebase-98327.firebaseapp.com",
+    databaseURL: "https://rps-firebase-98327.firebaseio.com",
+    projectId: "rps-firebase-98327",
+    storageBucket: "rps-firebase-98327.appspot.com",
+    messagingSenderId: "115629299844"
+};
+firebase.initializeApp(config);
+
+let database = firebase.database();
